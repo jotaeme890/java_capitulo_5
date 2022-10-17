e19ef3020346c6e4b247dabf0d86a75932bf1a3b
jotaeme890 <permanentjavi@gmail.com>
2022-10-17 19:04:47 +0200

    Ejercicio1

---
+++ Ejercicio1.java
@@ -0,0 +1,19 @@
+/*
+ * 1 Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle for.
+ * 
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio1{
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Vamos a mostrar los números de 5 en 5 hasta llegar al 100: ");
+
+        for(int i = 0; i<=100; i =i+5){
+            System.out.println(i);
+        }
+    }
+}
