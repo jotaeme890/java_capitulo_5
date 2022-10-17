69af9039566bcb25187a00acce0ad5e9bc889504
jotaeme890 <permanentjavi@gmail.com>
2022-10-17 19:10:25 +0200

    Ejercicio2

---
+++ Ejercicio2.java
@@ -0,0 +1,21 @@
+import javax.print.DocFlavor.STRING;
+
+/*
+ * 2 Muestra los números múltiplos de 5 de 0 a 100 utilizando un bucle while.
+
+ * 
+ * @author jotaeme
+ */
+
+public class Ejercicio2 {
+    public static void main(String args []){
+        System.out.println("Vamos a mostrar los números de 5 en 5 hasta llegar al 100 con un bucle while: ");
+
+        int i = 0;
+
+        while (i<=100) {
+            System.out.println(i);
+            i=i+5;
+        }
+    }
+}
