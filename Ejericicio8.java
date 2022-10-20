ec0bdc5274dde07a90e327916a4905554c243299
jotaeme890 <permanentjavi@gmail.com>
2022-10-20 10:01:45 +0200

    Ejercicio8

---
+++ Ejericicio8.java
@@ -0,0 +1,20 @@
+/**
+ * 8 Muestra la tabla de multiplicar de un número introducido por teclado.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejericicio8 {
+    public static void main (String args[]){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime la tabla de multiplicar de un número: ");
+        int n = sc.nextInt();
+        
+        for(int i = 0; i<=10; i++){
+            System.out.println("La tabla sería: " + (i*n));
+        }
+    sc.close();
+    }
+}
