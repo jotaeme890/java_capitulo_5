2a8b4722c2540c93f8dec4956ec75ce3d06cdfbc
jotaeme890 <permanentjavi@gmail.com>
2022-10-23 19:46:44 +0200

    Ejercicio53

---
+++ Ejercicio53.java
@@ -0,0 +1,25 @@
+/**
+ * 53 Realiza un programa que pinte un triángulo relleno tal como se muestra en los
+ * ejemplos. El usuario debe introducir la altura de la figura.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio53 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime la altura: ");
+        int n = sc.nextInt();
+        
+
+        for (int i = n ; i>=1 ; i--){
+            for (int asteriscos=1; asteriscos<=i; asteriscos++){
+                System.out.print("X");
+            }
+            System.out.println("");
+        }
+    sc.close();
+    }
+}
