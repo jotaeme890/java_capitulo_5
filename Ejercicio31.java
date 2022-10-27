f41e0d78fa665f2d5be0a35f9fb1fe107c78d3a5
jotaeme890 <permanentjavi@gmail.com>
2022-10-27 16:35:06 +0200

    Ejercicio31

--- Ejercicio31.java
+++ Ejercicio31.java
@@ -17,7 +17,7 @@ public class Ejercicio31  {
         for(int i = 1 ; i< n ; i++){
             System.out.println("*");
         }
-        for(int i = 0 ; i< n/2-1 ; i++){
+        for(int i = 0 ; i< n/2+1 ; i++){
             System.out.print("* ");
         }
         
