38cd7d1d87d6e39de59db4700ecadd9f77bbd6ec
jotaeme890 <permanentjavi@gmail.com>
2022-10-28 10:27:43 +0200

    Ejercicio47

---
+++ Ejercicio47.java
@@ -0,0 +1,47 @@
+/**
+ *  47 Con motivo de la celebración del día de la mujer, el 8 de marzo, nos han
+ * encargado realizar un programa que pinte un 8 por pantalla usando la letra
+ * M. Se pide al usuario la altura, que debe ser un número entero impar mayor
+ * o igual que 5. Si el número introducido no es correcto, el programa deberá
+ * mostrar un mensaje de error. A continuación se muestran algunos ejemplos.
+ * La anchura de la figura siempre será de 6 caracteres.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio47 {
+    public static void main(String args []){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime la altura: ");
+        int n = sc.nextInt();
+
+        System.out.print("MMMMMM");
+        System.out.println("");
+
+        for(int i = 1; i <= n/3; i++){
+            System.out.print("M");
+            for (int asteriscos = 1; asteriscos < 5 ; asteriscos ++){
+                System.out.print(" ");
+            }
+            System.out.print("M");
+            System.out.println("");
+        }
+        
+        System.out.print("MMMMMM");
+        System.out.println("");
+
+        for(int i = 1; i <= n/3; i++){
+            System.out.print("M");
+            for (int asteriscos = 1; asteriscos < 5 ; asteriscos ++){
+                System.out.print(" ");
+            }
+            System.out.print("M");
+            System.out.println("");
+        }
+
+        System.out.print("MMMMMM");
+        System.out.println("");
+    }
+}
