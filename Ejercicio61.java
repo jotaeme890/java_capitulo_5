586a4a18c53af5939bb09d52bc53388cf1e1601e
jotaeme890 <permanentjavi@gmail.com>
2022-10-28 14:26:18 +0200

    Ejercicio61

---
+++ Ejercicio61.java
@@ -0,0 +1,32 @@
+/**
+ * 61 Escribe un programa que pinte por pantalla la letra V. El ancho del palo de
+ * la V es siempre de 3 asteriscos. El usuario debe introducir la altura. La altura
+ * mínima es de 3 pisos. Si el usuario introduce una altura menor, el programa
+ * debe mostrar un mensaje de error.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio61 {
+    public static void main (String args []){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime la altura: ");
+        int n = sc.nextInt();
+        for (int i = 1 ; i <= n ; i++) /*Altura*/{
+            for (int espacioslat = 0 ; espacioslat <= i-1 ; espacioslat++){
+                System.out.print(" ");
+            }
+
+            System.out.print("***");
+
+            for(int espaciosint = (n-i)*2 ; espaciosint > 0 ; espaciosint--){
+                System.out.print(" ");
+            }
+
+            System.out.println("***");
+        }
+        sc.close();
+    }
+}
