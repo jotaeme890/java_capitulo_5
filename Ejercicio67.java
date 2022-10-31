101aafb39a9264854ca936b482ba8186bfffb7a5
jotaeme890 <permanentjavi@gmail.com>
2022-10-31 15:59:10 +0100

    Ejercicio67

---
+++ Ejercicio67.java
@@ -0,0 +1,29 @@
+/**
+ * 67 Realiza un programa que pinte una escalera que va descendiendo de izquierda
+ * a derecha. El programa pedirá el número de escalones y la altura de cada
+ * escalón. La anchura de los escalones siempre es la misma: 4 asteriscos.
+ * 
+ * @author jotaeme
+ */
+
+import java.util.Scanner;
+
+public class Ejercicio67 {
+    public static void main (String args []){
+        Scanner sc = new Scanner (System.in);
+        System.out.print("Dime la cantidad de escalones: ");
+        int n = sc.nextInt();
+        System.out.print("Dime la altura de los escalones: ");
+        int altura = sc.nextInt();
+
+        for(int i=1;i<=n;i++){
+            for(int b=1;b<=altura;b++){
+                for(int c=1;c<=i;c++){
+                    System.out.print("****");
+                }
+                System.out.println("");
+            }
+        }
+        sc.close();
+    }
+}
