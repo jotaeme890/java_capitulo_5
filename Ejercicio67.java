0d3ac27d54a0bc54a6343a5b9684a8dc236f68ca
jotaeme890 <permanentjavi@gmail.com>
2022-11-02 12:48:17 +0100

    Ejercicio67

--- Ejercicio67.java
+++ Ejercicio67.java
@@ -16,9 +16,9 @@ public class Ejercicio67 {
         System.out.print("Dime la altura de los escalones: ");
         int altura = sc.nextInt();
 
-        for(int i=1;i<=n;i++){ //altura total
-            for(int b=1;b<=altura;b++){ //altura escalon
-                for(int c=1;c<=i;c++){ //pintamos asteriscos
+        for(int i=1 ; i<=n ; i++){ //altura total
+            for(int b=1 ; b<=altura ; b++){ //altura escalon
+                for(int c=1 ; c<=i ; c++){ //pintamos asteriscos
                     System.out.print("****");
                 }
                 System.out.println("");
