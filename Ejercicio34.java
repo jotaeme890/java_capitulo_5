d50c81e743e25275e8da58b7cb4b46b27d72f16f
jotaeme890 <permanentjavi@gmail.com>
2022-11-03 09:20:07 +0100

    Ejercicio34

--- Ejercicio34.java
+++ Ejercicio34.java
@@ -13,7 +13,7 @@
 
 import java.util.Scanner;
 
-public class Ejercicio34 {
+public class Ejercicio34 { 
     public static void main (String args []){
         Scanner sc = new Scanner (System.in);
         System.out.print("Dime un número: ");
