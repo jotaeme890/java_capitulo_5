44177a7e8c248a5dd3c7284e82bb2412c6e96cfd
jotaeme890 <permanentjavi@gmail.com>
2022-11-07 09:26:11 +0100

    Ejercicio48

--- Ejercicio48.java
+++ Ejercicio48.java
@@ -15,119 +15,42 @@ public class Ejercicio48{
         System.out.print("Dime un número: ");
         long numero = sc.nextLong();
 
-        boolean cero=false;
-        boolean uno=false;
-        boolean dos=false;
-        boolean tres=false;
-        boolean cuatro=false;
-        boolean cinco=false;
-        boolean seis=false;
-        boolean siete=false;
-        boolean ocho=false;
-        boolean nueve=false;
-        
-        while(numero > 0){
-            if(numero%10 == 0){
-                cero=true;
-            }
-            if(numero%10 == 1){
-                uno=true;
-            }
-            if(numero%10 == 2){
-                dos=true;
-            }
-            if(numero%10 == 3){
-                tres=true;
-            }
-            if(numero%10 == 4){
-                cuatro=true;
-            }
-            if(numero%10 == 5){
-                cinco=true;
-            }
-            if(numero%10 == 6){
-                seis=true;
-            }
-            if(numero%10 == 7){
-                siete=true;
-            }
-            if(numero%10 == 8){
-                ocho=true;
-            }
-            if(numero%10 == 9){
-                nueve=true;
-            }
-            numero/=10;
-        }
-        
-        System.out.print("Los numeros que aparecen son: ");
+        long aux;
+        aux=numero;
+        boolean salir = true;
 
-        if(cero == true){
-            System.out.print("0");
-        }
-        if(uno == true){
-            System.out.print("1");
-        }
-        if(dos == true){
-            System.out.print("2");
-        }
-        if(tres == true){
-            System.out.print("3");
-        }
-        if(cuatro == true){
-            System.out.print("4");
-        }
-        if(cinco == true){
-            System.out.print("5");
-        }
-        if(seis == true){
-            System.out.print("6");
-        }
-        if(siete == true){
-            System.out.print("7");
-        }
-        if(ocho == true){
-            System.out.print("8");
-        }
-        if(nueve == true){
-            System.out.print("9");
+        System.out.print("Los dígitos que hay en ese número son: ");
+        for(int i = 0; i<10; i++){
+            salir=true;
+            aux=numero;
+            while(aux>0 && salir){
+                if(i == aux%10){ //si i, que empieza valiendo 0 es igual al último del dígito que sale, sale del bucle y lo pinta ya que aparece en el número
+                    salir=false;
+                }
+                aux/=10;
+            }
+            if(!salir){
+                System.out.print(i + " ");
+            }
         }
 
         System.out.println("");
 
-        System.out.print("Los numeros que no aparecen son: ");
-
-        if(cero == false){
-            System.out.print("0");
+        System.out.print("Los dígitos que no hay en ese número son: ");
+        for(int i = 0; i<10; i++){
+            salir=true;
+            aux=numero;
+            while(aux>0 && salir){
+                if(i == aux%10){
+                    salir=false;
+                }
+                aux/=10;
+            }
+            if(salir){
+                System.out.print(i + " ");
+            }
         }
-        if(uno == false){
-            System.out.print("1");
-        }
-        if(dos == false){
-            System.out.print("2");
-        }
-        if(tres == false){
-            System.out.print("3");
-        }
-        if(cuatro == false){
-            System.out.print("4");
-        }
-        if(cinco == false){
-            System.out.print("5");
-        }
-        if(seis == false){
-            System.out.print("6");
-        }
-        if(siete == false){
-            System.out.print("7");
-        }
-        if(ocho == false){
-            System.out.print("8");
-        }
-        if(nueve == false){
-            System.out.print("9");
-        }
+        
         sc.close();
     }
 }
-
